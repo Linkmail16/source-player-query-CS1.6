daec97fba6adeff5119d899aa66c7e49c8a46c38
Linkmail <103543983+Linkmail16@users.noreply.github.com>
2024-05-16 15:05:20 -0500

    Add files via upload

---
+++ serverDecodeFinal.py
@@ -0,0 +1,56 @@
+import socket
+import re
+
+def separar_texto(texto):
+    palabras = re.findall(r'\\([^\\]+)', texto)
+    return {i+1: palabra for i, palabra in enumerate(palabras)}
+
+def send_source_query(server_address, server_port, output_file):
+    try:
+       
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        
+        message = bytearray([
+            0xFF, 0xFF, 0xFF, 0xFF, 
+            ord('T'), ord('S'), ord('o'), ord('u'),
+            ord('r'), ord('c'), ord('e'), ord(' '), 
+            ord('E'), ord('n'), ord('g'), ord('i'), 
+            ord('n'), ord('e'), ord(' '), ord('Q'), 
+            ord('u'), ord('e'), ord('r'), ord('y'), 0x00 
+        ])
+
+       # print("Enviando mensaje:", message)
+        sock.sendto(message, (server_address, server_port))
+
+        response, _ = sock.recvfrom(1024)
+
+        with open(output_file, 'w') as file:
+           # textoServer = ("{}\n".format(response))
+            textoServer = ("{}".format(response))
+            texto = textoServer
+            palabras = separar_texto(texto)
+            for i, palabra in palabras.items():
+                if i == 5:
+                    palabra = palabra.replace("x00", "")
+                    print("Nombre:", palabra)
+                elif i == 6:
+                    palabra = palabra.replace("x00", "")
+                    print("Mapa:", palabra)
+                elif i == 10:
+                    palabra = palabra.replace("x", "")
+                    print("Número de jugadores:", palabra)
+                
+
+       
+        sock.close()
+    except Exception as e:
+        print("Error:", e)
+
+if __name__ == "__main__":
+    server_address = "45.32.170.143"  
+    server_port = 27015  
+    output_file = "output.txt" 
+
+    send_source_query(server_address, server_port, output_file)
+  
